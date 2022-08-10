e765ec9d5fcb4fc730f2077aea92526d0fd8a935
tiga <2517789608@qq.com>
2022-08-10 22:44:05 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contentbash].js'
-  }
+    filename: 'index.[contentbash].js'
+  },
+  plugins: [new HtmlWebpackPlugin({
+      title: 'xiao',
+      template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
