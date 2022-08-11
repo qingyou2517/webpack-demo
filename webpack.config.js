eab8be7f24d02f231f037a5bce421b9814555362
tiga <2517789608@qq.com>
2022-08-11 22:00:42 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,22 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
+  ...base,
+  devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist"
   },
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[contentbash].js'
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  plugins: [new HtmlWebpackPlugin({
-      title: 'xiao',
-      template: 'src/assets/index.html'
-  })],
   module: {
     rules: [
+      ...base.module.rules,
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
 };
