de840bb850289a13aa6c3d9fae612f3f8feae112
tiga <2517789608@qq.com>
2022-08-11 08:49:38 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,17 @@ var path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: "./dist"
+  },
   entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'index.[contentbash].js'
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   plugins: [new HtmlWebpackPlugin({
       title: 'xiao',
